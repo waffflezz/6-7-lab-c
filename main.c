9b7ba1dfebb669a26cb56730c5fcda3b8b209d5d
Dima Skorobogatov <dima.skorobogatov.99@mail.ru>
2022-01-24 09:55:16 +0700

    realise main function

--- main.c
+++ main.c
@@ -11,6 +11,7 @@ int main(void) {
     printf("Enter max array len:");
     scanf("%d", &array_len);
     array = malloc(array_len * sizeof(int));
+
     for (int i = 0; i < array_len; ++i) {
         array[i] = rand() % 20;
     }
@@ -20,9 +21,27 @@ int main(void) {
         printf("%d ", array[i]);
     }
 
-    printf("Maximum non-decreasing sequence\n");
-    for (int i = 0; i < array_len; ++i) {
+    printf("\n\n");
 
+    int s1 = 0, f1 = 0;
+    int s2 = 0, f2 = 0, diff = 0;
+
+    for (int i = 1; i < array_len; ++i) {
+        if (array[i] >= array[i - 1]) {
+            f2 = i;
+            diff = f2 - s2 + 1;
+            if (f1 - s1 + 1 <= diff) {
+                s1 = s2;
+                f1 = f2;
+            }
+        } else {
+            s2 = i;
+        }
+    }
+
+    printf("Maximum non-decreasing sequence\n");
+    for (int i = s1; i <= f1; ++i) {
+        printf("%d ", array[i]);
     }
 
 
