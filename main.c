793a913190a4f34f8520f598658f888066e2ffc5
Dima Skorobogatov <dima.skorobogatov.99@mail.ru>
2022-01-27 21:44:07 +0700

    add functions

--- main.c
+++ main.c
@@ -3,50 +3,73 @@
 #include <time.h>
 
 //15 OP
+void sequence_border(const int array[], int array_len, int *start, int *end);
+void print_sequence(int array[], int start, int end);
 
-
-int main(void) {
+int main(void)
+{
     int *array;
     int array_len;
 
     srand(time(NULL));
 
-    printf("Enter max array len:");
+    puts("Enter max array len:");
     scanf("%d", &array_len);
     array = malloc(array_len * sizeof(int));
 
-    for (int i = 0; i < array_len; ++i) {
+    for (int i = 0; i < array_len; ++i)
+    {
         array[i] = rand() % 20;
     }
 
-    printf("Numbers in massive:\n");
-    for (int i = 0; i < array_len; ++i) {
+    puts("Numbers in massive:");
+    for (int i = 0; i < array_len; ++i)
+    {
         printf("%d ", array[i]);
     }
 
-    printf("\n\n");
+    puts("\n");
 
+    int start, end;
+    sequence_border(array, array_len, &start, &end);
+
+    printf("Maximum non-decreasing sequence\n");
+    print_sequence(array, start, end);
+
+    free(array);
+
+    return 0;
+}
+
+void sequence_border(const int array[], int array_len, int *start, int *end)
+{
     int s1 = 0, f1 = 0;
     int s2 = 0, f2, diff;
 
-    for (int i = 1; i < array_len; ++i) {
-        if (array[i] >= array[i - 1]) {
+    for (int i = 1; i < array_len; ++i)
+    {
+        if (array[i] >= array[i - 1])
+        {
             f2 = i;
             diff = f2 - s2 + 1;
             if (f1 - s1 + 1 <= diff) {
                 s1 = s2;
                 f1 = f2;
             }
-        } else {
+        } else
+        {
             s2 = i;
         }
     }
 
-    printf("Maximum non-decreasing sequence\n");
-    for (int i = s1; i <= f1; ++i) {
+    *start = s1;
+    *end = f1;
+}
+
+void print_sequence(int array[], int start, int end)
+{
+    for (int i = start; i <= end; ++i)
+    {
         printf("%d ", array[i]);
     }
-
-
-    return 0;
 }
