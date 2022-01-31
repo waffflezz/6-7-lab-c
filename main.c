d5eef7ebf5c6eaf80e4ac9657457a6ec43c2db0c
Dima Skorobogatov <dima.skorobogatov.99@mail.ru>
2022-01-31 14:15:59 +0700

    Add menu

--- main.c
+++ main.c
@@ -11,53 +11,81 @@ int input_int_check(char value_array[]);
 int input_int();
 
 int main(void) {
-    int *array;
+    int *array = NULL;
     int array_len;
+    int start, end;
 
     //update seed for random function
     srand(time(NULL));
 
-    puts("Enter max array len:");
-    array_len = input_int();
-    array = malloc(array_len * sizeof(int));
+    while (1) {
+        puts("1. Enter array len and fill it with random numbers");
+        puts("2. Find maximum non-decreasing sequence");
+        puts("3. Print array");
+        puts("4. Exit");
 
-    for (int i = 0; i < array_len; ++i) {
-        array[i] = rand() % 20;
-    }
+        int menu_input = input_int();
+        switch (menu_input) {
+            case 1:
+                puts("Enter array len:");
+                array_len = input_int();
 
-    puts("Numbers in massive:");
-    if (array_len < 100) {
-        for (int i = 0; i < array_len; ++i) {
-            printf("%d ", array[i]);
+                if (array != NULL)
+                    free(array);
+
+                array = malloc(array_len * sizeof(int));
+
+                for (int i = 0; i < array_len; ++i) {
+                    array[i] = rand() % 20;
+                }
+
+                break;
+            case 2:
+                printf("%p\n", array);
+                if (array != NULL) {
+                    sequence_border(array, array_len, &start, &end);
+
+                    printf("Maximum non-decreasing sequence\n");
+                    print_sequence(array, start, end);
+                } else {
+                    fputs("First, specify the length of the array and fill it with numbers\n", stderr);
+                }
+
+                break;
+            case 3:
+                if (array != NULL) {
+                    puts("Numbers in massive:");
+                    if (array_len < 100) {
+                        for (int i = 0; i < array_len; ++i) {
+                            printf("%d ", array[i]);
+                        }
+                        puts("");
+                    } else {
+                        for (int i = 0; i < 100; ++i) {
+                            printf("%d ", array[i]);
+                        }
+                        puts("...");
+                    }
+                } else {
+                    fputs("First, specify the length of the array and fill it with numbers\n", stderr);
+                }
+
+                break;
+            case 4:
+                free(array);
+                exit(0);
+            default:
+                fputs("Not in menu\n", stderr);
+                break;
         }
-    } else {
-        for (int i = 0; i < 100; ++i) {
-            printf("%d ", array[i]);
-        }
-        printf("...");
     }
-
-    puts("\n");
-
-    int start, end;
-    sequence_border(array, array_len, &start, &end);
-
-    printf("Maximum non-decreasing sequence\n");
-    print_sequence(array, start, end);
-
-    free(array);
-
-
-
-
-    return 0;
 }
 
 int input_int() {
     char str_array_len[MAX_INPUT_ARRAY_LEN];
     fgets(str_array_len, MAX_INPUT_ARRAY_LEN, stdin);
     if (!(input_int_check(str_array_len)) || strlen(str_array_len) == 1) {
-        fputs("You enter incorrect value. Please, enter integer!\n", stderr);
+        fputs("You enter incorrect value!\n", stderr);
         return -1;
     }
 
@@ -89,6 +117,7 @@ void print_sequence(int array[], int start, int end) {
     for (int i = start; i <= end; ++i) {
         printf("%d ", array[i]);
     }
+    puts("");
 }
 
 int input_int_check(char value_array[]) {
